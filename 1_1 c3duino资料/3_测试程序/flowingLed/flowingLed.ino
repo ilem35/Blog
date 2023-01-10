300191d360d4ac6b6a53bd532004729749918283
ilem35 <102783767+ilem35@users.noreply.github.com>
2023-01-10 15:08:07 +0800

    Add files via upload

--- 1_1 c3duino资料/3_测试程序/flowingLed/flowingLed.ino
+++ 1_1 c3duino资料/3_测试程序/flowingLed/flowingLed.ino
@@ -1,1 +1,21 @@
+#define D4 12
+#define D5 13
 
+void setup() {
+  Serial.begin(115200);
+  delay(500);           //经测试:初始化完后至少290ms内无法使用,这是内置usb初始速度慢导致的;这里延时500ms更稳定些
+  Serial.println("Hello World!");
+   
+  pinMode(D4,OUTPUT);
+  pinMode(D5,OUTPUT);
+}
+
+void loop() {
+  digitalWrite(D4,HIGH);
+  digitalWrite(D5,LOW);
+  delay(500);
+
+  digitalWrite(D4,LOW);
+  digitalWrite(D5,HIGH);
+  delay(500);          
+}                      
